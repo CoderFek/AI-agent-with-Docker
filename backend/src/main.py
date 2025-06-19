42650c219e909cecf840c714741f1e2f74f1fc02
CoderFek <praveen.anand2212@gmail.com>
2025-06-19 19:02:29 +0530

    injecting passwords-secrets at runtime

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
+MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' is not set")
+
 @app.get("/")
 def get_index():
-    return {"message": "Hello world"}
+    return {"message": "Hello world", "project":MY_PROJECT, "api_key": API_KEY}
